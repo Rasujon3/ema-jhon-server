24eba04d3f208a0f99ade27929cd86d59cbe26b7
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-09-19 12:38:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ const port = 5000
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("products");
 
     app.post('/addProduct',(req,res)=>{
         const products = req.body;
         console.log(products)
-        productsCollection.insertMany(products)
+        productsCollection.insertOne(products)
             .then(result => {
                 console.log(result.insertedCount);
                 res.send(result.insertedCount);
@@ -47,6 +48,14 @@ client.connect(err => {
           .toArray((err,documents) =>{
               res.send(documents);
           })
+    })
+
+    app.post('/addOrder',(req,res)=>{
+        const order = req.body;
+        ordersCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     })
 
 });
